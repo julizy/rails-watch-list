1271aff0d8c7ace72166ef8701174c7a0f4421ca
Ye Zhu <dellazhuye@gmail.com>
2022-08-06 14:40:08 +0800

    image upload for list done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
 Movie.destroy_all
 
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
@@ -14,6 +16,7 @@ Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal masterm
 
 List.destroy_all
 
-List.create(name: 'Drama')
-List.create(name: 'Girl Power')
-List.create(name: 'Science fiction')
+file = URI.open("https://centre-stage.com/home/wp-content/uploads/2014/11/Creative-Drama-1.jpg")
+list = List.new(name: 'Drama')
+list.photo.attach(io: file, filename: "Creative-Drama-1.jpg", content_type: "image/jpg")
+list.save
